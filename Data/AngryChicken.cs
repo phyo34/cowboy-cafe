8d6a71e03864686c90e341e531c63ff1950b1873
May <phyo@ksu.edu>
2020-01-29 14:52:41 -0600

    2 Done.

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Public class 
+    /// </summary>
+    public class AngryChicken
+    {
+        /// <summary>
+        /// The price of the Angry Chicken 
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 5.99; 
+            }
+        }
+
+        /// <summary>
+        /// The calories of the Angry Chicken
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 190;
+            }
+        }
+        /// <summary>
+        /// Ensures that the Angry Chicken intially has bread
+        /// </summary>
+        private bool bread = true;
+       
+        /// <summary>
+        /// If the Angry Chicken should be served with bread 
+        /// </summary>
+        public bool Bread
+        {
+            get { return bread; }
+            set { bread = value; }
+        }
+
+        /// <summary>
+        /// If the Angry Chicken should be served with pickles 
+        /// </summary>
+        public bool Pickle { get; set; } = true; 
+
+        /// <summary>
+        /// Provides special instructions for the Angry Chicken
+        /// </summary>
+        public List<String> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                if(!Bread)
+                {
+                    instructions.Add("hold bread");
+                }
+                if (!Pickle)
+                {
+                    instructions.Add("hold pickle"); 
+                }
+
+                return instructions; 
+            }
+        }
+
+        
+
+
+    }
+}
---
+++ Data/RustlersRib.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class RustlersRib
+    {
+        //Make list empty for no instructions 
+        /// <summary>
+        /// The price of the rib
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories of the rib 
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+
+    }
+}
